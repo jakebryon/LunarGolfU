25fe356950863d487b8cc956b28e212a63ec786a
jakebryon <jakebryon1995@berkeley.edu>
2018-03-06 23:31:32 -0800

    rocket placement

--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -4,10 +4,39 @@ using UnityEngine;
 
 public class RocketController : MonoBehaviour {
 	public GameObject blackHole;
+	public GameObject planet;
 	public float gravityFactor = 1f;
 	public bool released = false;
+	public bool placement = true;
+	public float Theta = 0f;
+	public float Phi = 0f;
 
 	void FixedUpdate () {
+		if (placement) {
+			var p1 = planet.transform.TransformPoint(0, 0, 0);
+			var p2 = planet.transform.TransformPoint(1, 1, 0);
+			var w = 5.5*(p2.x - p1.x);
+			Vector3 Rad = new Vector3((float)w*Mathf.Cos(Theta), (float)w*Mathf.Sin(Theta), 0);
+
+			transform.position = planet.transform.position + Rad;
+			Quaternion temp = transform.rotation;
+			temp.eulerAngles = new Vector3(0,0,Phi);
+			transform.rotation = temp;
+				
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				Theta += .1f;
+			}
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				Theta += -.1f;
+			}
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				Phi += 1f;
+			}
+			if (Input.GetKey (KeyCode.DownArrow)) {
+				Phi += -1f;
+			}
+		}
+
 		if (released) {
 			Rigidbody2D rocketrb = GetComponent<Rigidbody2D> ();
 			Rigidbody2D blackHolerb = blackHole.GetComponent<Rigidbody2D> ();
