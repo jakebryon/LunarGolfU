e2b49b050d8f37b2cf6ed2cf9178039e5e24b4c8
Andrew Halle <ahalle@berkeley.edu>
2018-03-06 20:32:46 -0800

    added a master controller which turns off the global gravity

---
+++ Assets/Scripts/MasterController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Physics2D.gravity = Vector2.zero;
+	}
+
+}
