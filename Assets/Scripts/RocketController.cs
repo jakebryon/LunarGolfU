3977335fd9d5a0e4570ee4046b55d4fade732856
Andrew Halle <ahalle@berkeley.edu>
2018-03-06 21:19:41 -0800

    rocket controller now has a released boolean

---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour {
+	public GameObject blackHole;
+	public float gravityFactor = 1f;
+	public bool released = false;
+
+	void FixedUpdate () {
+		if (released) {
+			Rigidbody2D rocketrb = GetComponent<Rigidbody2D> ();
+			Rigidbody2D blackHolerb = blackHole.GetComponent<Rigidbody2D> ();
+			rocketrb.AddForce ((blackHole.transform.position - transform.position).normalized
+			* blackHolerb.mass * gravityFactor /
+			(blackHole.transform.position - transform.position).sqrMagnitude);
+		}
+	}
+}
