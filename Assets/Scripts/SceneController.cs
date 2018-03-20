23b0edf148f3746612f0f9d92d9e700c146aefd0
Andrew Halle <ahalle@berkeley.edu>
2018-03-20 12:16:39 -0700

    added a simpe button which moves between scenes

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour {
+
+	public Button startButton;
+
+	// Use this for initialization
+	void Start () {
+		Button b = startButton.GetComponent<Button> ();
+		b.onClick.AddListener (ChangeToSelectCourse);
+	}
+	
+	void ChangeToSelectCourse() {
+		SceneManager.LoadScene ("Scenes/course_select", LoadSceneMode.Single);
+	}
+}
